8f8c63bcf62a0924e9fb4d5e147e87fd6a169943
kwonsoobin63 <kwon.soobin63@gmail.com>
2026-08-29 14:30:52 +0900

    modify 4

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'rewear-v6';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './config.js', './firebase-auth.js', './manifest.webmanifest', './icon.svg', './icon-maskable.svg'];
+const ASSETS = ['./', './index.html', './style.css', './platform.css', './platform.js', './wardrobe.html', './current.html', './market.html', './fabric.html', './config.js', './firebase-auth.js', './manifest.webmanifest', './icon.svg', './icon-maskable.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {

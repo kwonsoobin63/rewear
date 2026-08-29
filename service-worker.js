5845f3ce3375f247cb478427e19f7f7b2faa0432
kwonsoobin63 <kwon.soobin63@gmail.com>
2026-08-29 14:08:40 +0900

    fix web cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rewear-v5';
+const CACHE_NAME = 'rewear-v6';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './config.js', './firebase-auth.js', './manifest.webmanifest', './icon.svg', './icon-maskable.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
